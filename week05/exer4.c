39b20ea4f0f222cbe8f5863c368d5b2c791de4d3
fbehddn <tom0611@naver.com>
2023-03-31 10:31:10 +0900

    ++x And y++

---
+++ week05/exer4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 10, y = 20;
+    printf("x=%d\n", x);
+    printf("++x의 값은 :%d\n", ++x);
+    printf("x=%d\n\n", x);
+
+    printf("y=%d\n", y);
+    printf("y++의 값은 :%d\n", y++);
+    printf("y=%d\n\n", y);
+}
