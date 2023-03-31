04e95de1c0c2a1413cd33a6ef036398ff1f31d67
fbehddn <tom0611@naver.com>
2023-03-31 10:31:32 +0900

    restMoney

---
+++ week05/exer5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int price, money;
+    int restMoney;
+
+    printf("물건값을 입력하시오: ");
+    scanf("%d", &price);
+    printf("\n투입 금액 입력하시오: ");
+    scanf("%d", &money);
+    printf("\n거스름돈은 다음과 같습니다.\n\n");
+
+    restMoney = money - price;
+    printf("1000원 %d 장\n500원 동전 %d 개\n1000원 동전 %d개", restMoney / 1000, (restMoney % 1000) / 500, ((restMoney % 1000) % 500) / 100);
+}
